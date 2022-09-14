cc7d445fb463e6ea7b500e41d850f6396a587c61
Nick <nickyrick@gmail.com>
2022-09-14 15:30:29 -0400

    Added blackjack to games

--- games/blackjack.py
+++ games/blackjack.py
@@ -1,16 +1,26 @@
 import random
-from nextcord import User
-class Blackjack():
+from typing import Text
+from discord import Interaction
+from nextcord import User, TextChannel
+import nextcord
+class Blackjack(nextcord.ui.View):
+    children = []
     deck = {"Ace of Spades": 1, "Two of Spades": 2, "Three of Spades": 3, "Four of Spades": 4, "Five of Spades": 5, "Six of Spades": 6, "Seven of Spades": 7, "Eight of Spades": 8, "Nine of Spades": 9, "Ten of Spades": 10, "Jack of Spades": 10, "Queen of Spades": 10, "King of Spades": 10, "Ace of Hearts": 1, "Two of Hearts": 2, "Three of Hearts": 3, "Four of Hearts": 4, "Five of Hearts": 5, "Six of Hearts": 6, "Seven of Hearts": 7, "Eight of Hearts": 8, "Nine of Hearts": 9, "Ten of Hearts": 10, "Jack of Hearts": 10, "Queen of Hearts": 10, "King of Hearts": 10, "Ace of Clubs": 1, "Two of Clubs": 2, "Three of Clubs": 3, "Four of Clubs": 4, "Five of Clubs": 5, "Six of Clubs": 6, "Seven of Clubs": 7, "Eight of Clubs": 8, "Nine of Clubs": 9, "Ten of Clubs": 10, "Jack of Clubs": 10, "Queen of Clubs": 10, "King of Clubs": 10, "Ace of Diamonds": 1, "Two of Diamonds": 2, "Three of Diamonds": 3, "Four of Diamonds": 4, "Five of Diamonds": 5, "Six of Diamonds": 6, "Seven of Diamonds": 7, "Eight of Diamonds": 8, "Nine of Diamonds": 9, "Ten of Diamonds": 10, "Jack of Diamonds": 10, "Queen of Diamonds": 10, "King of Diamonds": 10}
-    def __init__(self, user: User, wager: int) -> None:
+    def __init__(self, user: User, wager: int, channel: TextChannel) -> None:
+        super().__init__(timeout=600)
         self._user = user
         self._wager = wager
         self._player_hand = []
         self._dealer_hand = []
         self.game_deck = []
+        self._won = False
+        self.channel = channel
         for i in range(3):
             self.game_deck.append(self.deck.keys())
         random.shuffle(self.game_deck)
+
+    def setup(self):
+        pass
 
     @property
     def user(self):
@@ -27,6 +37,10 @@ class Blackjack():
     @property
     def dealer_hand(self):
         return self._dealer_hand
+    
+    @property
+    def won(self):
+        return self._won
 
     @property
     def get_total(self, player: bool) -> int:
@@ -42,23 +56,53 @@ class Blackjack():
     def start(self):
         pass
 
-    def hit(self, player: bool) -> int:
+    @nextcord.ui.button(label="Hit", style=nextcord.ButtonStyle.green)
+    def hit(self, interaction: Interaction) -> int:
         index = random.randint(0, len(self.game_deck))
         card = self.game_deck.pop(index)
-        if player:
-            self._player_hand.append(card)
-            if self.is_blackjack(True):
-                self.gameover()
-            return self.deck[card]
+        if card == 1:
+            card = card if sum(self._player_hand) + 11 > 21 else 11
+        self._player_hand.append(card)
 
-    def stand(self):
+    @nextcord.ui.button(label="Stand", style=nextcord.ButtonStyle.red)
+    def stand(self, interaction: nextcord.Interaction):
         while self.get_total(False) < 17:
+            index = random.randint(0, len(self.game_deck))
+            card = self.game_deck.pop(index)
+            self._dealer_hand.append(card)
             self.hit(False)
-        self.gameover()
 
+    def isGameover(self):
+        if self.busted(True) or self.busted(False) or self.is_blackjack(True) or self.is_blackjack(False):
+            self.gameover()
+            return True
+
+    def busted(self, player: bool):
+        if player:
+            if self.get_total(True) > 21:
+                return True
+        else:
+            if self.get_total(False) > 21:
+                return True
+        return False
 
     def is_blackjack(self, player: bool) -> bool:
         if player:
-            return self.get_total(True) == 21
+            winner = self.get_total(True) == 21
+            if winner:
+                self._won = True
+            return winner
         else:
             return self.get_total(False) == 21
+
+    def gameover(self):
+        if self.won:
+            if self.wager:
+                self.channel.send(f"{self.user.mention} won {self.wager} coins!")
+            else:
+                self.channel.send(f"{self.user.mention} won!")
+        else:
+            if self.wager:
+                self.channel.send(f"{self.user.mention} lost {self.wager} coins!")
+            else:
+                self.channel.send(f"{self.user.mention} lost!")
